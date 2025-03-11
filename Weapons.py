1ffbff741e245b41275d88b4213f2ffcd03a6db1
MrCGW <105022534+MrCGW@users.noreply.github.com>
2025-03-11 19:14:23 +1300

    Added weapon choice functionality
    Created a .py file containing a weapon dictionary.
    Contents read into the character creation script.

---
+++ Weapons.py
@@ -0,0 +1,16 @@
+
+weapon = {
+    "Shortsword": 5,
+    "Axe": 7,
+    "Spear": 6,
+    "Halberd": 5,
+    "Club": 8,
+    "Dagger": 3,
+    "Quarterstaff": 4,
+    "Longsword": 6,
+    "Mace": 7
+}
+#print(weapon)
+
+if __name__ == "__main__": #checks if the program is run directly or called from another script
+    print(weapon)
