ba4181c402dca5e21276094e1b3d44204fadb4e9
craig.walker <craig.walker@christscollege.com>
2025-04-04 12:45:03 +1300

    Created GUIBattleScreen.py: Set up left and right frame, started placement of widgets.
    Left Frame will be used for player actions, right frame for animation canvas.
    
    Bottom frame to display player stats of health and damage output.

--- GUIBattleScreen.py
+++ GUIBattleScreen.py
@@ -4,6 +4,43 @@
 
 import tkinter as tk
 
+# Set initial positions of x and y coordinates + the spacing between instances of stickmen.
+initial_x = 100
+initial_y = 50
+spacing = 100
+
+def draw_stickman(canvas, x1, y1):
+    # Constants for sizes of elements.
+    HEAD_SIZE = 40
+    BODY_LTH = 40
+    ARM_SPAN = 60
+    LEG_LTH = 50
+
+    # Head
+    head_tl = (x1, y1) # Start top left corner
+    head_br = (x1 + HEAD_SIZE, y1 + HEAD_SIZE) # bottom right relative to top left.
+
+    # Body
+    body_start = x1 + HEAD_SIZE/2, y1 + HEAD_SIZE
+    body_end = (x1 + HEAD_SIZE/2, y1 + HEAD_SIZE + BODY_LTH)
+
+
+
+    # Arms
+    # canvas.create_line(80, 120, 140, 120, fill="black", width=2)
+
+    # Legs
+    # canvas.create_line(110, 170, 80, 220, fill="black", width=2)
+    # canvas.create_line(110, 170, 140, 220, fill="black", width=2)
+
+    # Draw Head
+    # Takes arguments from coordinates (stored and referenced as a list
+    canvas.create_oval(head_tl[0], head_tl[1], head_br[0], head_br[1], outline="black", width=2)
+
+    # Draw body
+    canvas.create_line(body_start[0], body_start[1], body_end[0], body_end[1], fill="black", width=2)
+
+
 # functions
 def attack():
     return
@@ -18,6 +55,7 @@ window.geometry("750x500")
 window.title("Battle")
 window.resizable(False, False)
 
+# Frame Geometry
 frm_left_frame = tk.Frame(relief=tk.SUNKEN, borderwidth=3, width=350, height=250)
 frm_left_frame.grid(row=1, column=1, pady=10, padx=10)
 frm_left_frame.grid_propagate(False)
@@ -26,8 +64,9 @@ frm_right_frame = tk.Frame(relief=tk.SUNKEN, borderwidth=3, width=350, height=25
 frm_right_frame.grid(row=1, column=2, padx=10, pady=10)
 frm_right_frame.grid_propagate(False)
 
-frm_bottom_frame = tk.Frame(relief=tk.SUNKEN, borderwidth=3)
-frm_bottom_frame.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
+frm_bottom_frame = tk.Frame(relief=tk.SUNKEN, borderwidth=3,width=720, height=50)
+frm_bottom_frame.grid(row=2, column=1, columnspan=2, padx=10, pady=10)
+frm_bottom_frame.grid_propagate(False)
 
 # Left Frame widgets
 lbl_actions = tk.Label(
@@ -47,6 +86,10 @@ btn_heal.grid(row=4, column=1, padx=5, pady=5, sticky='w'+'e'+'n'+'s')
 cv = tk.Canvas(master=frm_right_frame, bg="lightblue", width=325, height=225)
 cv.grid(row=1, column=1, sticky="nsew", padx=5, pady=5)
 
+draw_stickman(cv, initial_x, initial_y)
+draw_stickman(cv, initial_x + spacing, initial_y)
 
-
+# Bottom Frame widgets
+lbl_health = tk.Label(master=frm_bottom_frame, bg="green", text="health")
+lbl_health.grid(row=1, column=3, padx=10, pady=10)
 window.mainloop()
